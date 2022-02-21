0f4afc1e4e3d02c937b89d4013194210a2bfe3e9
Paul Brunet <paulbrunet@live.fr>
2022-02-21 16:42:12 +0100

    2nd commit

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,16 @@
 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(13,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("J'ALLUME !!!");
+  digitalWrite(13,HIGH);
+  delay(1000);
+  digitalWrite(13,LOW);
+   Serial.println("J'ETEINS !!!");
+  delay(1000);
 }
